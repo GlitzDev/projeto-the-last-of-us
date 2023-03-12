75ab25dfc6dbe1ae0602f8e3cd2e31f3d10ca2b7
Gustavo Sousa de Novais <119053161+GlitzDev@users.noreply.github.com>
2023-03-12 17:15:32 -0300

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,34 @@
+const botoesCarrosel = document.querySelectorAll('.botao');
+const imagens = document.querySelectorAll('.imagem');
+
+botoesCarrosel.forEach((botao, indice) => {
+    botao.addEventListener('click', () => {
+
+        desativarBotaoSelecionado();
+
+        selecionarBotaoCarrosel(botao);
+
+        esconderImagemAtiva();
+
+        mostarImagemdeFundo(indice);
+       
+        })
+    })
+function mostarImagemdeFundo(indice) {
+    imagens[indice].classList.add('ativa');
+}
+
+function selecionarBotaoCarrosel(botao) {
+    botao.classList.add('selecionado');
+}
+
+function esconderImagemAtiva() {
+    const imagemAtiva = document.querySelector('.ativa');
+    imagemAtiva.classList.remove('ativa');
+}
+
+function desativarBotaoSelecionado() {
+    const botaoSelecionado = document.querySelector('.selecionado');
+    botaoSelecionado.classList.remove('selecionado');
+}
+
